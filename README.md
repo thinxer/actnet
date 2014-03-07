dd028f0761f6c5e21adae48fe8b546476eb93b25
Jianfei Wang <me@thinxer.com>
2014-03-07 14:07:20 +0800

    update doc

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 #Activity Net
 
 This project is for extracting human daily activities from Weibo and other similiar data sources.
+
+A sample data set of weibo can be found [here](http://s.txr.me/weibo.tgz). This data set contains a sample of Weibo JSON, Segmented Weibo Text, and a POI dataset.
+
+Code document can be viewed at [godoc.org](https://godoc.org/github.com/thinxer/actnet).
--- doc.go
+++ doc.go
@@ -6,20 +6,20 @@ It can be used to model/predict user behavior, discover new POIs, or help NLP in
 
 The workflow consists of the following parts.
 
-1. Process the raw data into the form required by the importer. There can be multiple importers.
-2. Use the importer to generate the universal intermediate representations of source data.
-3. Build the activity net from the source data. Revise this model using human labeled data.
-4. Query the activity net.
+  1. Process the raw data into the form required by the importer. There can be multiple importers.
+  2. Use the importer to generate the universal intermediate representations of source data.
+  3. Build the activity net from the source data. Revise this model using human labeled data.
+  4. Query the activity net.
 
 With this workflow, we can integrate different data sources into one.
 
 
 This package can answer the following questions:
 
-1. Are my extracted activities real human activities? (Classification)
-2. How does my extracted activities look like? (Visualization)
-2. Is there any new activities recently? (Trend)
-3. What's the relations between activities? (Relation)
+  1. Are my extracted activities real human activities? (Classification)
+  2. How does my extracted activities look like? (Visualization)
+  2. Is there any new activities recently? (Trend)
+  3. What's the relations between activities? (Relation)
 
 Note that this package do not focus on the recall/precision of the activity extraction.
 It's rather a tool for ultilizing the output of such process.
